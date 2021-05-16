169c1039b4bbe5c9a427604366ba9099d7f7c269
ybespalava <ybespalava@gmail.com>
2021-05-16 22:21:00 +0300

    feat: add async call for boards

---
+++ src/resources/boarsd/board.memory.repository.js
@@ -0,0 +1,8 @@
+const getAll = async () => dB.getAllBoards();
+const get = async (id) => dB.getBoard(id);
+const create = async (board) => dB.createBoard(board);
+const dB = require('../../common/inMemoryDb');
+const remove = async (board) => dB.removeBoard(board);
+const update = async (board) => dB.updateBoard(board);
+
+module.exports = { getAll, get, create, update, remove };
