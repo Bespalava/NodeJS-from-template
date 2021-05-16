35dd589f8deaadb43245b93aaf5f4cbfbcd7db75
ybespalava <ybespalava@gmail.com>
2021-05-16 23:05:43 +0300

    fix: call db

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,8 +1,8 @@
+const dB = require('../../common/inMemoryDb');
 const getAll = async () => dB.getAllUsers();
 const get = async (id) => dB.getUser(id);
 const create = async (user) => dB.createUser(user);
 const remove = async (id) => dB.removeUser(id);
 const update = async (user) => dB.updateUser(user);
-const dB = require('../../common/inMemoryDb');
 
 module.exports = { getAll, get, create, update, remove };
