68f64f1fa1159c26666577149e7c3b7d219928fa
ybespalava <ybespalava@gmail.com>
2021-05-28 14:42:59 +0300

    feat: add service

--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,2 +1,13 @@
-const usersRepo = require('./task.memory.repository');
-const User = require('./task.model');
+const tasksRepo = require('./tasks.memory.repository');
+
+const getAll = () => tasksRepo.getAll();
+
+const get = (id) => tasksRepo.get(id);
+
+const save = (task) => tasksRepo.save(task);
+
+const update = (id, task) => tasksRepo.update(id, task);
+
+const remove = (id) => tasksRepo.remove(id);
+
+module.exports = { getAll, get, save, update, remove };
