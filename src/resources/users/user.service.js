e390b85aa9ac123b4d0bd2f04c26c4ff4257af36
ybespalava <ybespalava@gmail.com>
2021-06-01 13:29:50 +0300

    fix: fix calls

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,21 +1,11 @@
 const usersRepo = require('./user.memory.repository');
 const User = require('./user.model');
 
-const getAll = async () => {
-  const users = await usersRepo.getAll();
-  return users.map(User.toResponse);
-};
-const get = async (id) => {
-  const user = await usersRepo.get(id);
-  if (user) return User.toResponse(user);
-  return undefined;
-};
-const create = async (userData) => {
-  const { name, login, password } = userData;
-  const newUser = new User({ name, login, password });
-  const createdUser = await usersRepo.create(newUser);
-  return User.toResponse(createdUser);
-};
+const getAll = () => usersRepo.getAll();
+
+const get = id => usersRepo.get(id);
+
+const create = user => usersRepo.create(user);
 
 const update = (userData) => {
   const { name, login, password, id } = userData;
@@ -23,6 +13,7 @@ const update = (userData) => {
   const updatedUser = usersRepo.update(user);
   return User.toResponse(updatedUser);
 };
+
 const remove = (id) => usersRepo.remove(id);
 
 module.exports = { getAll, get, create, update, remove };
